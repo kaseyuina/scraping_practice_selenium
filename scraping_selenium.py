4289383108c95879e002c334e0539902b49b9aa6
kaseyuina <tomx2nakaji@gmail.com>
2023-04-19 23:28:25 +0900

    First commit

---
+++ scraping_selenium.py
@@ -0,0 +1,19 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+chrome_path = os.environ.get('chrome_path')
+
+# print(chrome_path)
+options = Options()
+options.add_argument('--incognito')
+
+driver = webdriver.Chrome(executable_path=chrome_path, options=options)
+
+url = 'https://tech-diary.net'
+driver.get(url)
+
+sleep(5)
+
+driver.quit()
