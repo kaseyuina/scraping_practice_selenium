ce07c961b83ba8a9f16ee2877500a3a70b690cb6
kaseyuina <tomx2nakaji@gmail.com>
2023-04-21 00:08:38 +0900

    Get image link and store in csv file

--- scraping_selenium.py
+++ scraping_selenium.py
@@ -2,6 +2,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import pandas as pd
 import os
 
 # chrome_path = os.environ.get('chrome_path')
@@ -31,4 +32,29 @@ while height < 3000:
     print(height)
 
     sleep(1)
+
+# Select images
+elements = driver. find_elements(By.CLASS_NAME, ('sw-Thumbnail'))
+
+d_list = []
+for i, element in enumerate(elements, start=1):
+    name = f'{query}_{i}'
+    print(name)
+    raw_url = element.find_element(By.CLASS_NAME, 'sw-ThumbnailGrid__details').get_attribute('href')
+    yahoo_image_url = element.find_element(By.TAG_NAME, 'img').get_attribute('src')
+    title = element.find_element(By.TAG_NAME, 'img').get_attribute('alt')
+
+    d = {
+        'filename': name,
+        'raw_url': raw_url,
+        'yahoo_image_url': yahoo_image_url,
+        'title': title
+    }
+    d_list.append(d)
+
+    sleep(1)
+
+df = pd.DataFrame(d_list)
+df.to_csv('image_urls_20230420.csv')
+
 driver.quit()
