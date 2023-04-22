64fc9bfee8e4a57de51f52f2867d81668d578132
kaseyuina <tomx2nakaji@gmail.com>
2023-04-22 23:47:17 +0900

    Cleaned up some comments

--- scraping_selenium.py
+++ scraping_selenium.py
@@ -1,8 +1,9 @@
 from time import sleep
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import os
 
 import pandas as pd
 import requests
@@ -19,9 +20,8 @@ driver = webdriver.Chrome(executable_path=chrome_path, options=options)
 url = 'https://search.yahoo.co.jp/image'
 driver.get(url)
 
-sleep(3)
+sleep(2)
 
-# query = 'Cat'
 search_box = driver.find_element(By.CLASS_NAME, 'SearchBox__searchInput')
 search_box.send_keys(query)
 search_box.submit()
@@ -31,7 +31,6 @@ height = 1000
 while height < 3000:
     driver.execute_script("window.scrollTo(0, {});".format(height))
     height += 100
-    print(height)
 
     sleep(1)
 
@@ -53,9 +52,6 @@ for i, element in enumerate(elements, start=1):
         'title': title
     }
     d_list.append(d)
-
-    # sleep(1)
-
 
 df = pd.DataFrame(d_list)
 df.to_csv('image_urls_20230420.csv')
